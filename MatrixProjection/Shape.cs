a44d1b6801d2d7e2cca4482905fa2ff3ece26cc6
flavio <flaviosantos002010@hotmail.com>
2020-12-22 01:18:53 +0000

    Add a Shape class as a future parent class for all 3D objects

---
+++ MatrixProjection/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixProjection {
+
+    public abstract class Shape {
+
+        public abstract Vector[] Vertices { get; protected set; }
+
+        public abstract void DrawShape(Draw draw, Vector[] projected, bool render = true);
+    }
+}
