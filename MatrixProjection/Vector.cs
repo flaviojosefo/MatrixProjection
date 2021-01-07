147d9e4bb30ba1596daad94ea0914f7f9c2917ed
flavio <flaviosantos002010@hotmail.com>
2021-01-07 21:17:09 +0000

    Add new operator overrides for Vectors and methods to calculate magnitude

--- MatrixProjection/Vector.cs
+++ MatrixProjection/Vector.cs
@@ -12,11 +12,15 @@ namespace MatrixProjection {
         public float Y { get; private set; }
         public float Z { get; private set; }
 
+        public float Magnitude { get; private set; }
+
         public Vector (float x, float y, float z = 0) {
 
             X = x;
             Y = y;
             Z = z;
+
+            Magnitude = (float)Math.Sqrt((X * X) + (Y * Y) + (Z * Z));
         }
 
         public static Vector operator +(Vector v1, Vector v2) {
@@ -36,6 +40,21 @@ namespace MatrixProjection {
         public static Vector operator /(Vector v, float scalar) {
 
             return new Vector(v.X / scalar, v.Y / scalar, v.Z / scalar);
+        }
+
+        public static bool operator ==(Vector v1, Vector v2) {
+
+            return ((v1.X == v2.X) && (v1.Y == v2.Y) && (v1.Z == v2.Z));
+        }
+
+        public static bool operator !=(Vector v1, Vector v2) {
+
+            return ((v1.X != v2.X) || (v1.Y != v2.Y) || (v1.Z != v2.Z));
+        }
+
+        public static float Distance(Vector v1, Vector v2) {
+
+            return (v2 - v1).Magnitude;
         }
 
         public override string ToString() {
