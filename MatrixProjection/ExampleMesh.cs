9206437b24f03a27911de0e1a881b7b8e547bea6
flavio <flaviosantos002010@hotmail.com>
2021-01-26 12:25:04 +0000

    Add new Mesh class to later replace Shape (3D objects start having planes, not only vertices)

---
+++ MatrixProjection/ExampleMesh.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixProjection {
+
+    public class ExampleMesh : Mesh {
+
+        public ExampleMesh(float size) {
+
+            Polygons = new Vector[6][];
+
+            CreateQuad(new Vector(size * 0.5f, 0, 0),
+                       new Vector(0, -size * 0.5f, 0),
+                       new Vector(0, 0, size * 0.5f));
+
+            CreateQuad(new Vector(-size * 0.5f, 0, 0),
+                       new Vector(0, size * 0.5f, 0),
+                       new Vector(0, 0, size * 0.5f));
+
+            CreateQuad(new Vector(0, size * 0.5f, 0),
+                       new Vector(size * 0.5f, 0),
+                       new Vector(0, 0, size * 0.5f));
+
+            CreateQuad(new Vector(0, -size * 0.5f, 0),
+                       new Vector(-size * 0.5f, 0),
+                       new Vector(0, 0, size * 0.5f));
+
+            CreateQuad(new Vector(0, 0, size * 0.5f),
+                       new Vector(-size * 0.5f, 0),
+                       new Vector(0, size * 0.5f, 0));
+
+            CreateQuad(new Vector(0, 0, -size * 0.5f),
+                       new Vector(size * 0.5f, 0),
+                       new Vector(0, size * 0.5f, 0));
+        }
+    }
+}
---
+++ MatrixProjection/Mesh.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixProjection {
+
+    public abstract class Mesh {
+
+        // Mesh = Collection of polygons, which is a collection of vertices (tris or quads)
+        public Vector[][] Polygons { get; protected set; }
+
+        protected void CreateQuad(Vector origin, Vector axis0, Vector axis1) {
+
+            Vector[] quad = new Vector[4];
+
+            quad[0] = origin + axis0 + axis1;
+            quad[1] = origin - axis0 + axis1;
+            quad[2] = origin - axis0 - axis1;
+            quad[3] = origin + axis0 - axis1;
+
+            int firstEmpty = Array.IndexOf(Polygons, null);
+            Polygons[firstEmpty] = quad;
+        }
+
+        protected void CreateTri(Vector v1, Vector v2, Vector v3) {
+
+            Vector[] tri = new Vector[3];
+
+            tri[0] = v1;
+            tri[1] = v2;
+            tri[2] = v3;
+
+            int firstEmpty = Array.IndexOf(Polygons, null);
+            Polygons[firstEmpty] = tri;
+        }
+    }
+}
