dd9382abf3783c7e0174aa125f12ad7e4543f710
flavio <flaviosantos002010@hotmail.com>
2021-01-27 19:04:43 +0000

    Fix Matrix printing order

--- MatrixProjection/Matrix3D.cs
+++ MatrixProjection/Matrix3D.cs
@@ -107,7 +107,7 @@ namespace MatrixProjection {
 
                 for (int j = 0; j < Matrix.GetLength(1); j++) {
 
-                    mat += $"{Matrix[i, j]} ";
+                    mat += $"{Matrix[j, i]} ";
                 }
 
                 mat += '\n';
