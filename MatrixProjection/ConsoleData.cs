5d712cca85f598ab9996f80c83cb972e771a3e01
flavio <flaviosantos002010@hotmail.com>
2021-11-13 02:04:33 +0000

    Add ConsoleData struct to apply color information (already tested, needs optimizations)

---
+++ MatrixProjection/ConsoleData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MatrixProjection {
+
+    public struct ConsoleData {
+
+        public (int, int) XInterval { get; }
+        public int Y { get; }
+        public ConsoleColor Color { get; }
+
+        public ConsoleData((int, int) xInterval, int y, ConsoleColor color) {
+
+            XInterval = xInterval;
+            Y = y;
+            Color = color;
+        }
+
+        public int GetInterval() {
+
+            return XInterval.Item2 - XInterval.Item1;
+        }
+    }
+}
