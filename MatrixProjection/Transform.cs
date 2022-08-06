8fb57cba15efda18d615b29a5695bfbd6edeef1f
fs000 <flaviosantos002010@hotmail.com>
2022-08-06 18:02:11 +0100

    Add Transform to store object position, rotation and scale

---
+++ MatrixProjection/Transform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MatrixProjection {
+
+    public class Transform {
+
+        public Vector Position { get; set; }
+        public Vector Rotation { get; set; }
+        public Vector Scale { get; set; } = new Vector(1, 1, 1);
+
+        public Transform() { }
+
+        public void Move(Vector translation) => Position += translation;
+
+        public void Rotate(Vector rotation) => Rotation += rotation;
+    }
+}
