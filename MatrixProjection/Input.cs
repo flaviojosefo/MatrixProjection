bdbb97bfc57743adb7c3090fd2e26933d384670e
fs000 <flaviosantos002010@hotmail.com>
2022-08-06 18:08:00 +0100

    Add empty Input class

---
+++ MatrixProjection/Input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MatrixProjection {
+
+    public class Input {
+
+        private Camera camera;
+
+        public Input(Camera camera) {
+
+            this.camera = camera;
+        }
+
+        public void ManageInput() {
+
+
+        }
+    }
+}
