33b02f84678c6c22f3b6fbd2b0cabf8d50a34d91
fs000 <flaviosantos002010@hotmail.com>
2022-08-14 22:31:06 +0100

    Add a new Mesh representing a simple camera facing triangle

--- MatrixProjection/Menu.cs
+++ MatrixProjection/Menu.cs
@@ -30,7 +30,8 @@ namespace MatrixProjection {
             Console.WriteLine("  2.Quad Pyramid");
             Console.WriteLine("  3.Triangular Pyramid");
             Console.WriteLine("  4.Engram (Regular Dodecahedron)");
-            Console.WriteLine("  5.Exit");
+            Console.WriteLine("  5.Simple Triangle");
+            Console.WriteLine("  6.Exit");
 
             Console.SetCursorPosition(0, optionN);
             Console.Write('►');
@@ -38,11 +39,11 @@ namespace MatrixProjection {
             switch(Console.ReadKey().Key) {
 
                 case ConsoleKey.UpArrow:
-                    optionN = optionN > 0 ? optionN - 1 : 4;
+                    optionN = optionN > 0 ? optionN - 1 : 5;
                     break;
 
                 case ConsoleKey.DownArrow:
-                    optionN = optionN < 4 ? optionN + 1 : 0;
+                    optionN = optionN < 5 ? optionN + 1 : 0;
                     break;
 
                 case ConsoleKey.Enter:
@@ -78,6 +79,10 @@ namespace MatrixProjection {
                     break;
 
                 case 5:
+                    scene = new Scene(60, RenderObject.Create<SimpleTri>());
+                    break;
+
+                case 6:
                     Environment.Exit(0);
                     return;
             }
---
+++ MatrixProjection/SimpleTri.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MatrixProjection {
+
+    public class SimpleTri : Mesh {
+
+        public override Triangle[] Polygons { get; protected set; } = new Triangle[1];
+
+        public SimpleTri() {
+
+            // The center of the triangle
+            Vector3 triCenter = new Vector3();
+
+            // The distance from the center to each vertex
+            float dist = 2f;
+
+            // The top vertex from which the other 2 will be created
+            Vector3 firstVertex = triCenter + new Vector3(0, dist);
+
+            // The vertex to the right of the top vertex
+            Vector3 secondVertex = RotateVertex(firstVertex, -120.0f);
+
+            // The vertex to the left of the top vertex
+            Vector3 thirdVertex = RotateVertex(firstVertex, 120.0f);
+
+            // Create the triangle adding the vertices in a clockwise direction
+            CreateTri (
+                firstVertex,
+                secondVertex,
+                thirdVertex
+            );
+        }
+
+        // Rotates a vertices' x and y coords
+        private Vector3 RotateVertex(Vector3 vertex, float angle) {
+
+            float angleRad = angle * (float)(Math.PI / 180.0f);
+
+            float rotX = (vertex.X * (float)Math.Cos(angleRad)) - (vertex.Y * (float)Math.Sin(angleRad));
+            float rotY = (vertex.Y * (float)Math.Cos(angleRad)) - (vertex.X * (float)Math.Sin(angleRad));
+
+            return new Vector3(rotX, rotY, vertex.Z);
+        }
+    }
+}
