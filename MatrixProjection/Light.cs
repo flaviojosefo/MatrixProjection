1886d0e04e7d8af7dbc4bdb52a6778033427af32
fs000 <flaviosantos002010@hotmail.com>
2022-08-15 16:03:37 +0100

    Light's direction now always becomes normalized when changed

--- MatrixProjection/Light.cs
+++ MatrixProjection/Light.cs
@@ -6,15 +6,16 @@ namespace MatrixProjection {
 
         public Vector3 Position { get; set; }
 
-        public Vector3 Direction { get; set; }
+        private Vector3 direction;
+        public Vector3 Direction { get => direction; set => direction = value.Normalized; }
 
         public float Intensity { get; set; }
 
         public Light(Vector3 position, float intensity = 1.0f) {
 
             Position = position;
+            direction = new Vector3(0, -1, 0);
             Intensity = intensity;
-            Direction = new Vector3(0, -1, 0);
         }
     }
 }
