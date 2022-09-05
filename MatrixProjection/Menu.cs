458ad6567c79d67b76f72871b39a6fcb54443a2f
flavio <flaviosantos002010@hotmail.com>
2022-09-05 02:05:49 +0100

    Create submenus which let the user choose if they want to start a scene with project objects or external 'obj' files

--- MatrixProjection/Menu.cs
+++ MatrixProjection/Menu.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace MatrixProjection {
 
     public class Menu {
 
-        private const string CONSOLE_TITLE = "MatrixProjection";
+        private const string PROJECT_TITLE = "MatrixProjection";
+        private const string OBJ_DIRECTORY = "Models";
 
-        private List<string> options;
-        private int optionN;
+        private readonly string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        private List<string> mainOptions;
+        private List<string> localObjects;
 
         public Menu(int width, int height) {
 
@@ -21,92 +24,260 @@ namespace MatrixProjection {
 
             Console.CursorVisible = false;
 
-            Console.Title = CONSOLE_TITLE;
+            Console.Title = PROJECT_TITLE;
         }
 
         // Create menu options
         public void Setup() {
 
-            options = new List<string> {
+            // The options on the "main menu"
+            mainOptions = new List<string> {
+                "Local Objects",
+                "Import OBJ",
+                "Exit"
+            };
+
+            // The options on the "local" submenu
+            localObjects = new List<string> {
                 "Cube",
                 "Quad Pyramid",
                 "Triangular Pyramid",
                 "Engram (Regular Dodecahedron)",
                 "Simple Triangle",
-                "Exit"
+                "Back"
             };
+
+            // Create a directory to store external files at startup
+            CreateFilesDirectory();
         }
 
-        public void ShowOptions() {
+        public void DisplayMainOptions(int cursor = 0) {
 
             // Clean the console
             Console.Clear();
 
             // Get the number of options
-            int maxOptions = options.Count;
+            int maxOptions = mainOptions.Count;
 
             // Print every option (+ its index)
             for (int i = 0; i < maxOptions; i++)
-                Console.WriteLine($"  {i + 1}." + options[i]);
+                Console.WriteLine($"  {i + 1}." + mainOptions[i]);
 
-            // Print 'cursor'
-            Console.SetCursorPosition(0, optionN);
+            // Print the 'cursor'
+            Console.SetCursorPosition(0, cursor);
             Console.Write('►');
 
             // Read user input
             switch(Console.ReadKey().Key) {
 
                 case ConsoleKey.UpArrow:
-                    optionN = optionN > 0 ? optionN - 1 : maxOptions - 1;
+                    cursor = cursor > 0 ? cursor - 1 : maxOptions - 1;
                     break;
 
                 case ConsoleKey.DownArrow:
-                    optionN = optionN < maxOptions - 1 ? optionN + 1 : 0;
+                    cursor = cursor < maxOptions - 1 ? cursor + 1 : 0;
                     break;
 
                 case ConsoleKey.Enter:
-                    SelectOption();
+                    SelectMainOption(cursor);
                     break;
             }
 
-            ShowOptions();
+            DisplayMainOptions(cursor);
         }
 
-        private void SelectOption() {
+        private void SelectMainOption(int cursor) {
+
+            switch (cursor) {
+
+                case 0:
+                    DisplayLocal();
+                    break;
+
+                case 1:
+                    SearchFiles();
+                    break;
+
+                case 2:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        // ********** SUBMENUS **********
+
+        private void DisplayLocal(int cursor = 0) {
+
+            // Clean the console
+            Console.Clear();
+
+            // Get the number of options
+            int maxOptions = localObjects.Count;
+
+            // Print every option (+ its index)
+            for (int i = 0; i < maxOptions; i++)
+                Console.WriteLine($"  {i + 1}." + localObjects[i]);
+
+            // Print the 'cursor'
+            Console.SetCursorPosition(0, cursor);
+            Console.Write('►');
+
+            // Read user input
+            switch (Console.ReadKey().Key) {
+
+                case ConsoleKey.UpArrow:
+                    cursor = cursor > 0 ? cursor - 1 : maxOptions - 1;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    cursor = cursor < maxOptions - 1 ? cursor + 1 : 0;
+                    break;
+
+                case ConsoleKey.Enter:
+
+                    // Exit this submenu
+                    if (cursor == maxOptions - 1)
+                        return;
+
+                    StartLocalObject(cursor);
+
+                    break;
+            }
+
+            DisplayLocal(cursor);
+        }
+
+        private void StartLocalObject(int cursor) {
 
             Console.Clear();
 
             Scene scene = null;
 
-            switch(optionN + 1) {
+            switch (cursor) {
 
-                case 1:
+                case 0:
                     scene = new Scene(RenderObject.Create<Cube>());
                     break;
 
-                case 2:
+                case 1:
                     scene = new Scene(RenderObject.Create<QuadPyramid>());
                     break;
 
-                case 3:
+                case 2:
                     scene = new Scene(RenderObject.Create<TriPyramid>());
                     break;
 
-                case 4:
+                case 3:
                     scene = new Scene(RenderObject.Create<Engram>());
                     break;
 
-                case 5:
+                case 4:
                     scene = new Scene(RenderObject.Create<SimpleTri>());
                     break;
-
-                case 6:
-                    Environment.Exit(0);
-                    return;
             }
 
             scene?.Start();
             scene?.Run();
+        }
+
+        // Creates the directory where 'obj' files can be placed
+        private void CreateFilesDirectory() {
+
+            // Combine folder names
+            string path = Path.Combine(documents, PROJECT_TITLE, OBJ_DIRECTORY);
+
+            // Check if the path provided exists
+            if (!Directory.Exists(path)) {
+
+                // Create the 'obj' folder if not found
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        // Searches for 'obj' files
+        private void SearchFiles() {
+
+            // Combine folder names
+            string path = Path.Combine(documents, PROJECT_TITLE, OBJ_DIRECTORY);
+
+            // Prevent bugs related to directory deletion while the application is running
+            CreateFilesDirectory();
+
+            // Get all 'obj' files on the directory
+            string[] objPaths = Directory.GetFiles(path, "*.obj", SearchOption.TopDirectoryOnly);
+
+            DisplayFiles(objPaths);
+        }
+
+        private void DisplayFiles(string[] paths, int cursor = 0) {
+
+            // Clean the console
+            Console.Clear();
+
+            // Get the number of files
+            int filesAmount = paths.Length;
+
+            // Check if any files were found
+            if (filesAmount != 0) {
+
+                // Print every file name (+ its index)
+                for (int i = 0; i < filesAmount; i++)
+                    Console.WriteLine($"  {i + 1}." + paths[i].Split('\\').Last()/*.Split('.').First()*/);
+
+            } else {
+
+                // Print a message to the user
+                Console.WriteLine($" No files found.");
+
+                // Lock the cursor on 'back'
+                cursor = 1;
+            }
+
+            // Print the 'back' option
+            Console.WriteLine("  Back");
+
+            // Print the 'cursor'
+            Console.SetCursorPosition(0, cursor);
+            Console.Write('►');
+
+            // Read user input
+            switch (Console.ReadKey().Key) {
+
+                case ConsoleKey.UpArrow:
+                    cursor = cursor > 0 ? cursor - 1 : filesAmount;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    cursor = cursor < filesAmount ? cursor + 1 : 0;
+                    break;
+
+                case ConsoleKey.Enter:
+
+                    // Exit this submenu
+                    if (filesAmount == 0 || 
+                        cursor == filesAmount)
+                        return;
+
+                    // Load the selected file
+                    for (int i = 0; i < filesAmount; i++) {
+
+                        // Get the file's index
+                        if (cursor == i) {
+
+                            SelectFile(paths[i]);
+                            break;
+                        }
+                    }
+
+                    break;
+            }
+
+            DisplayFiles(paths, cursor);
+        }
+
+        private void SelectFile(string path) {
+
+            // Mesh Importer, scene setter, etc.
         }
     }
 }
